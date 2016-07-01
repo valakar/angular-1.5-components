a90ae4d5de6677afe9352938d53a58be1cd452cf
Viktor Novak <valakar@gmail.com>
2016-07-01 16:48:41 +0300

    [Part4] - fix codecov

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
                     {
                         test: /\.js$/,
                         loader: 'isparta',
-                        exclude: /node_modules|\.spec.js$|\.conf.js$/
+                        exclude: /node_modules|\.spec.js$|\.config.js$/
                     },
                     { test: /\.html$/, loader: 'raw' },
                     { test: /\.jade$/, loader: 'jade-loader' },
